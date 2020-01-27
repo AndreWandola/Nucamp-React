0f7b8c000f0396b0c801cc2399d7a71dd9b570bd
Andrew Wandola <andrewandola93@gmail.com>
2020-01-26 16:18:17 -0800

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Directory from './components/DirectoryComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
+        <Navbar dark color="primary" />
+        <div className="container">
+          <NavbarBrand href="/">NuCamp</NavbarBrand>
+        </div>
+        <Directory />
       </div>
     );
   }
